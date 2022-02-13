8075376fd49fd7ef632871ff2f6ceaff52827679
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-02-13 17:49:19 +0000

    Change all instances of BarNotes to MenuNote

--- utils/note.py
+++ utils/note.py
@@ -4,11 +4,11 @@ user_name = getpass.getuser()
 
 class Note:
     def get_note():
-        return open(f"/users/{user_name}/Library/Application Support/BarNotes/note.txt").read()
+        return open(f"/users/{user_name}/Library/Application Support/MenuNote/note.txt").read()
 
     def set_note(note):
         if note == "":
-            note = "Bar Notes"
+            note = "Menu Note"
         note = note[:100]
-        with open(f"/users/{user_name}/Library/Application Support/BarNotes/note.txt", "w") as f:
+        with open(f"/users/{user_name}/Library/Application Support/MenuNote/note.txt", "w") as f:
             f.write(note)
