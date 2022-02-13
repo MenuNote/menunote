d1cf134c4ddac3b42fe8155731619f4b76ff33d5
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-02-13 17:48:46 +0000

    Change all instances of BarNotes to MenuNote

--- utils/filecheck.py
+++ utils/filecheck.py
@@ -7,21 +7,21 @@ def check_files():
     loadingwindow = LoadingWindow()
 
     files = [
-        "https://raw.githubusercontent.com/bentettmar/barnotes/main/data/icon.png",
-        "https://raw.githubusercontent.com/bentettmar/barnotes/main/data/note.txt"
+        "https://raw.githubusercontent.com/MenuNote/menunote/main/data/icon.png",
+        "https://raw.githubusercontent.com/MenuNote/menunote/main/data/note.txt"
     ]
 
     loadingwindow.update_text("Checking files...")
-    if not os.path.exists(os.path.expanduser("~/Library/Application Support/BarNotes")):
-        os.makedirs(os.path.expanduser("~/Library/Application Support/BarNotes"))
+    if not os.path.exists(os.path.expanduser("~/Library/Application Support/MenuNote")):
+        os.makedirs(os.path.expanduser("~/Library/Application Support/MenuNote"))
         loadingwindow.update_text("Creating directory...")
 
     loadingwindow.update_text("Downloading files...")
     for file in files:
-        if not os.path.exists(os.path.expanduser(f"~/Library/Application Support/BarNotes/{file.split('/')[-1]}")):
+        if not os.path.exists(os.path.expanduser(f"~/Library/Application Support/MenuNote/{file.split('/')[-1]}")):
             loadingwindow.update_text(f"Downloading {file.split('/')[-1]}...")
             r = requests.get(file)
-            with open(os.path.expanduser(f"~/Library/Application Support/BarNotes/{file.split('/')[-1]}"), "wb") as f:
+            with open(os.path.expanduser(f"~/Library/Application Support/MenuNote/{file.split('/')[-1]}"), "wb") as f:
                 f.write(r.content)
             loadingwindow.update_text(f"Downloaded {file.split('/')[-1]}...")
     
